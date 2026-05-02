1b2b37fe52f94abc52d6009b696a165877224223
Lluis Morales Dobon <lmorales@cameparkare.com>
2026-05-02 12:54:10 +0200

    building backbone for logical MCU i2c driver

--- Drivers/CommsLogicalDrivers/I2C/c/logic_i2c.c
+++ Drivers/CommsLogicalDrivers/I2C/c/logic_i2c.c
@@ -1,65 +1,153 @@
+#include "C:\Users\MAX PC\Documents\repositories\CodePool\Common\MCU\global_def.h"
 #include "logic_i2c.h"
 
-TLogicI2cConfig Logical_I2C_Config_Handler;
+//
+TLogicI2cConfig Logical_I2C_Port_Config_Handler[MAX_DEF_PORTS];
+TI2C_Status I2CPortStatus[MAX_DEF_PORTS];
 
 //*****************************************************************************
-TI2C_Status Logical_I2C_Config(TLogicI2cConfig *Logical_I2C_Config_Handler)
+TI2C_Status Logical_I2C_PortInit(UINT8 i2cPort)
 //*****************************************************************************
 //
 //*****************************************************************************
 {
-	return I2C_OK;
+	if(i2cPort <= MAX_DEF_PORTS)
+	{
+		if(i2c_Init(i2cPort) != ERROR)
+		{
+			return I2CPortStatus[i2cPort] = I2C_PORT_INITIALIZED;
+		}
+		else
+		{
+			return I2CPortStatus[i2cPort] = I2C_ERROR_INIT_OPERATION;
+		}
+	}
+	else
+	{
+		return I2CPortStatus[i2cPort] = I2C_UNREACHABLE_BUS;
+	}
 }
 
 //*****************************************************************************
-TI2C_Status Logical_I2C_Start(void)
+TI2C_Status Logical_I2C_Stop(UINT8 i2cPort)
 //*****************************************************************************
 //
 //*****************************************************************************
 {
-	return I2C_OK;
+	if(i2cPort <= MAX_DEF_PORTS)
+	{
+		if(i2c_Stop(i2cPort) != ERROR)
+		{
+			return I2CPortStatus[i2cPort] = I2C_PORT_STOP;
+		}
+		else
+		{
+			return I2CPortStatus[i2cPort] = I2C_ERROR_STOP_OPERATION;
+		}
+	}
+	else
+	{
+		return I2CPortStatus[i2cPort] = I2C_UNREACHABLE_BUS;
+	}
 }
 
 //*****************************************************************************
-TI2C_Status Logical_I2C_Stop(void)
+TI2C_Status Logical_I2C_PortConfig(UINT8 i2cPort, TLogicI2cConfig Logical_I2C_Config_Handler[])
 //*****************************************************************************
 //
 //*****************************************************************************
 {
-	return I2C_OK;
+	if(i2cPort <= MAX_DEF_PORTS)
+	{
+		//Configuring the port
+		if( i2c_Config(	Logical_I2C_Port_Config_Handler[i2cPort].PortNumber,
+												Logical_I2C_Port_Config_Handler[i2cPort].busSpeed,
+												Logical_I2C_Port_Config_Handler[i2cPort].I2CTimeout) != ERROR)
+		{
+			return I2CPortStatus[i2cPort] = I2C_PORT_CONFIGURED;
+		}
+		else
+		{
+			return I2CPortStatus[i2cPort] = I2C_ERROR_CONFIG_OPERATION;
+		}
+	}
+	else
+	{
+		return I2CPortStatus[i2cPort] = I2C_UNREACHABLE_BUS;
+	}
 }
 
 //*****************************************************************************
-TI2C_Status Logical_I2C_Restart(void)
+TI2C_Status Logical_I2C_Restart(UINT8 i2cPort)
 //*****************************************************************************
 //
 //*****************************************************************************
 {
-	return I2C_OK;
+	if(i2cPort <= MAX_DEF_PORTS)
+	{
+		if(i2c_SendRestart(i2cPort) != ERROR)
+		{
+			return I2CPortStatus[i2cPort] = I2C_LAST_TRANSACTION_OK;
+		}
+		else
+		{
+			return I2CPortStatus[i2cPort] = I2C_ERROR_RESTART_OPERATION;
+		}
+	}
+	else
+	{
+		return I2CPortStatus[i2cPort] = I2C_UNREACHABLE_BUS;
+	}
 }
 
 //*****************************************************************************
-TI2C_Status Logical_I2C_SendByte(UINT8 DeviceAddr, UINT8 dataByte)
+TI2C_Status Logical_I2C_WriteData(UINT8 i2cPort, UINT8 deviceAddr, UINT8 reg, UINT8 *dataBuff, UINT8 count)
 //*****************************************************************************
 //
 //*****************************************************************************
 {
-	return I2C_OK;
+	UINT8 opstatus=ERROR;
+
+	if(i2cPort <= MAX_DEF_PORTS)
+	{
+		opstatus = i2c_WriteData(i2cPort, deviceAddr, reg, dataBuff, count);
+
+		switch (opstatus)
+		{
+			case I2C_LAST_TRANSACTION_OK:							return I2CPortStatus[i2cPort] = I2C_WRITE_OPERATION_OK;	break;
+			case I2C_LAST_TRANSACTION_ERROR_TIMEOUT: 	return I2CPortStatus[i2cPort] = I2C_ERROR_TIMEOUT; 			break;
+			case I2C_LAST_TRANSACTION_ERROR_ACK: 			return I2CPortStatus[i2cPort] = I2C_ERROR_ACK; 					break;
+			default:														 			return I2CPortStatus[i2cPort] = I2C_ERROR_UNKNOWN;			break;
+		}
+	}
+	else
+	{
+		return I2CPortStatus[i2cPort] = I2C_UNREACHABLE_BUS;
+	}
 }
 
 //*****************************************************************************
-TI2C_Status Logical_I2C_SendByte(UINT8 DeviceAddr, UINT8 dataByte)
+TI2C_Status Logical_I2C_ReadData(UINT8 i2cPort, UINT8 deviceAddr, UINT8 reg, UINT8 *dataBuff, UINT8 count)
 //*****************************************************************************
 //
 //*****************************************************************************
 {
-	return I2C_OK;
+	UINT8 opstatus=ERROR;
+
+	if(i2cPort <= MAX_DEF_PORTS)
+	{
+		opstatus = i2c_ReadData(i2cPort, deviceAddr, reg, dataBuff, count);
+
+		switch (opstatus)
+		{
+			case I2C_LAST_TRANSACTION_OK:							return I2CPortStatus[i2cPort] = I2C_READ_OPERATION_OK;	break;
+			case I2C_LAST_TRANSACTION_ERROR_TIMEOUT: 	return I2CPortStatus[i2cPort] = I2C_ERROR_TIMEOUT; 			break;
+			case I2C_LAST_TRANSACTION_ERROR_ACK: 			return I2CPortStatus[i2cPort] = I2C_ERROR_ACK; 					break;
+			default:														 			return I2CPortStatus[i2cPort] = I2C_ERROR_UNKNOWN;			break;
+		}
+	}
+	else
+	{
+		return I2CPortStatus[i2cPort] = I2C_UNREACHABLE_BUS;
+	}
 }
-
-
-
-
-
-
-
-
--- Drivers/CommsLogicalDrivers/I2C/c/logic_i2c.h
+++ Drivers/CommsLogicalDrivers/I2C/c/logic_i2c.h
@@ -1,22 +1,47 @@
 #ifndef LOGIC_I2C_DRV
 #define LOGIC_I2C_DRV
 
-#include "typedef.h"
+#include "C:\Users\MAX PC\Documents\repositories\CodePool\Common\MCU\typedef.h"
 
 //Low level functions to be implemented in physical driver
-extern UINT8 ll_i2c_start(void);
-extern UINT8 ll_i2c_stop(void);
-extern UINT8 ll_i2c_write(UINT8 data);
-extern UINT8 ll_i2c_read(UINT8 *data, UINT8 ack);
-extern UINT8 ll_i2c_bus_busy(void);
+extern UINT8 i2c_Config(UINT8 i2cPort, UINT8 clkSpeed, UINT32 timeout);
+extern UINT8 i2c_Init(UINT8 i2cPort);
+extern UINT8 i2c_Stop(UINT8 i2cPort);
+
+extern UINT8 i2c_WriteData(UINT8 i2cPort, UINT8 addr, UINT8 reg, UINT8 *dataBuff, UINT8 Count);
+extern UINT8 i2c_ReadData(UINT8 i2cPort, UINT8 addr, UINT8 reg, UINT8 *dataBuff, UINT8 Count);
+
+extern UINT8 i2c_SendRestart(UINT8 i2cPort);
+extern UINT8 i2c_BusyCheck(UINT8 i2cPort);
+
+//max number of i2c independent ports to be controlled
+#define MAX_DEF_PORTS 8
+
+#define I2C_LAST_TRANSACTION_OK 						0
+#define I2C_LAST_TRANSACTION_ERROR_TIMEOUT 	1
+#define I2C_LAST_TRANSACTION_ERROR_ACK 			2
+#define I2C_LAST_TRANSACTION_ERROR_UNKNOWN 	99
 
 typedef enum
 {
-	I2C_OK = 0,
-	I2C_ERROR_TIMEOUT,
-	I2C_ERROR_NACK,
-	I2C_ERROR_BUS
+	I2C_PORT_NOT_INITIALIZED,
+	I2C_PORT_INITIALIZED,
+	I2C_PORT_CONFIGURED,
+	I2C_PORT_STOP,
 
+	I2C_ERROR_INIT_OPERATION,
+	I2C_ERROR_CONFIG_OPERATION,
+	I2C_ERROR_STOP_OPERATION,
+	I2C_ERROR_RESTART_OPERATION,
+	I2C_ERROR_WRITE_DATA_OPERATION,
+
+	I2C_WRITE_OPERATION_OK,
+	I2C_READ_OPERATION_OK,
+	I2C_ERROR_TIMEOUT,
+	I2C_ERROR_ACK,
+	I2C_ERROR_UNKNOWN,
+
+	I2C_UNREACHABLE_BUS
 }TI2C_Status;
 
 /**
@@ -39,7 +64,7 @@ typedef struct
 {
 	UINT8 				PortNumber;	/**<Physical i2c bus number >*/
 	TI2C_ClkSpeed	busSpeed;	/**<Physical i2c bus speed >*/
-	UINT8					I2CTimeout;	/**<Physical i2c timout to wait for response from slave>*/
+	UINT32				I2CTimeout;	/**<Physical i2c timout to wait for response from slave>*/
 
 }TLogicI2cConfig;
 
@@ -57,7 +82,7 @@ TI2C_Status Logical_I2C_Config(TLogicI2cConfig *Logical_I2C_Config_Handler);
  * @return TI2C_Status
  */
 //*****************************************************************************
-TI2C_Status Logical_I2C_Start(void);
+TI2C_Status Logical_I2C_Start(UINT8 i2cPort);
 //*****************************************************************************
 
 /**
@@ -65,7 +90,7 @@ TI2C_Status Logical_I2C_Start(void);
  * @return TI2C_Status
  */
 //*****************************************************************************
-TI2C_Status Logical_I2C_Stop(void);
+TI2C_Status Logical_I2C_Stop(UINT8 i2cPort);
 //*****************************************************************************
 
 /**
@@ -73,7 +98,32 @@ TI2C_Status Logical_I2C_Stop(void);
  * @return TI2C_Status
  */
 //*****************************************************************************
-TI2C_Status Logical_I2C_Restart(void);
+TI2C_Status Logical_I2C_Restart(UINT8 i2cPort);
 //*****************************************************************************
+
+/**
+ * @brief write byte
+ * @param[in] i2cPort: 		I2C Port number to send the data
+ * @param[in] deviceAddr: I2C device address
+ * @param[in] reg:				I2C device register to start writing data
+ * @param[in] dataBuff:		I2C duffer containing the bytes to write
+ * @param[in] count:			I2C number of bytes from dataBuff to write
+ * @return TI2C_Status
+ */
+//*****************************************************************************
+TI2C_Status Logical_I2C_WriteData(UINT8 i2cPort, UINT8 deviceAddr, UINT8 reg, UINT8 *dataBuff, UINT8 count);
+
+/**
+ * @brief write byte
+ * @param[in] i2cPort: 		I2C Port number from which read the data
+ * @param[in] deviceAddr: I2C device address
+ * @param[in] reg:				I2C device register to start read data
+ * @param[in] dataBuff:		I2C buffer to store the bytes read
+ * @param[in] count:			I2C number of bytes to read
+ * @return TI2C_Status
+ */
+//*****************************************************************************
+TI2C_Status Logical_I2C_ReadData(UINT8 i2cPort, UINT8 deviceAddr, UINT8 reg, UINT8 *dataBuff, UINT8 count);
+
 #endif
 
